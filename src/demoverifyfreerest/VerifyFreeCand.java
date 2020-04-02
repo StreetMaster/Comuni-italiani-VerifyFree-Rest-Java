726bd86122b4bcfb18fec265ba4fa74bec13f79b
StreetMaster <supporto@streetmaster.it>
2020-04-02 16:15:32 +0200

    Merge origin/master
    Conflicts:
    	README.md
    	build.xml
    	catalog.xml
    	lib/nblibraries.properties
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/jax-ws.xml
    	nbproject/jaxws-build.xml
    	nbproject/project.properties
    	nbproject/project.xml
    	src/META-INF/jax-ws-catalog.xml

---
+++ src/demoverifyfreerest/VerifyFreeCand.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demoverifyfreerest;
+
+/**
+ *
+ * @author toaiari
+ */
+public class VerifyFreeCand {
+    public String Prov;
+    public String Comune;
+    public String Frazione;
+    public String Cap;
+    public String Regione;
+    public String ProvEstesa;
+    public int ScoreComune;
+    public int ScoreStrada;
+}
---
+++ src/demoverifyfreerest/VerifyFreeResponse.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demoverifyfreerest;
+
+import java.util.List;
+
+/**
+ *
+ * @author toaiari
+ */
+
+public class VerifyFreeResponse {
+    public int Norm;
+    public int CodErr;
+    public int NumCand;
+    public List<VerifyFreeCand> Output;  
+}
